f6d2c458e5014565a945242255e95e3058afdf96
Aida Real <aida_real@epam.com>
2022-05-16 15:38:48 -0500

    Practice sort method

--- sort.js
+++ sort.js
@@ -1,7 +1,10 @@
 //Sort the following array alphabetically
 const pets = ['dog', 'cat', 'rabbit', 'hamster'];
+console.log(pets.sort());
 
 const grades = [88, 94, 72, 99, 53, 77];
 //Sort the following array from highest to lowest
+console.log(grades.sort((a,b)=>b-a));
 
 //Sort the following array from lowest to highest
+console.log(grades.sort());
