431b5ceb5a032c1d68c97f7efa66f955f4222c0e
Aida Real <areal060781@gmail.com>
2024-06-06 12:33:10 -0500

    Restore exercises

--- src/arrays/find_findIndex.js
+++ src/arrays/find_findIndex.js
@@ -1,9 +1,5 @@
 const grades = [99, 83, 87, 91, 77];
 
 //Find the first grade that is less than 80.
-const underEighty = grades.find(value => value < 80);
-console.log(underEighty);
 
 //Find the index of the first grade that is over 80.
-const overEighty = grades.findIndex(value => value > 80);
-console.log(overEighty);
--- src/arrays/reverse.js
+++ src/arrays/reverse.js
@@ -1,11 +1,6 @@
 const condiments = ['ketchup', 'mustard', 'relish', 'mayo'];
 // Use reverse to change the order of this array.
-console.log(condiments.reverse());
 
 const employees = ['Adams', 'Carter', 'Davis', 'Fraklin', 'Zappa'];
 // Use reverse to change the order of this array,
 // but keep the original array intact.
-const reversed = [...employees] //Array.from(employees);
-reversed.reverse();
-console.log(employees);
-console.log(reversed);
